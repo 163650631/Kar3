a5fd4f0be43ee5abdc32372c1bf2bde626d64c43
163650631 <1636506831@qq.com>
2026-05-01 16:20:51 +0800

    Update data.js

--- data.js
+++ data.js
@@ -6,7 +6,7 @@
 // ---- 网站基础设置 ----
 const SITE_CONFIG = {
     title: "未开放的资料库",
-    subtitle: "[ 限制访问 ] [ 误入请退出 ] [ 非法访问将追踪 ]"
+    subtitle: "[ 限制访问 ] [ 误入请退出 ] [ 非法访问将追踪 ]"字幕: “[ 限制访问 ] [ 误入请退出 ] [ 非法访问将追踪 ]”
 };
 
 // ---- 导航分区 ----
@@ -24,7 +24,7 @@ const POSTS = [
     { id: "history1", section: "history", title: "遗失的历史", poster: "[匿名]", desc: "■■■■■■■■■■ ■■■■■■■■■■ ■■■■", file: "history1.txt" },
     { id: "history2", section: "history", title: "薄雾街",     poster: "[匿名]", desc: "■■■■■■■■■■ ■■■■■■■■■■ ■■■■", file: "history2.txt" },
     { id: "history3", section: "history", title: "博爱之城",   poster: "[匿名]", desc: "■■■■■■■■■■ ■■■■■■■■■■ ■■■■", file: "history3.txt" },
-    { id: "history4", section: "history", title: "公司部门",   poster: "[匿名]", desc: "■■■■■■■■■■ ■■■■■■■■■■ ■■■■", file: "history4.txt" },
+    { id: "history4", section: "history", title: "公司部门",   poster: "[匿名]", desc: "■■■■■■■■■■ ■■■■■■■■■■ ■■■■", file: "history4.txt" },{ id: "history4", section: "历史", title: "公司部门",   poster: "[匿名]", desc: "■■■■■■■■■■ ■■■■■■■■■■ ■■■■", file: "history4.txt" },
 
     // ===== 白雾 =====
     { id: "fog1", section: "fog", title: "雾中的世界",   poster: "[匿名]", desc: "■■■■■■■■■■ ■■■■■■■■■■ ■■■■", file: "fog1.txt" },
@@ -33,7 +33,7 @@ const POSTS = [
     { id: "fog4", section: "fog", title: "？？？",       poster: "[未知]", desc: "■■■■■■■■■■ ■■■■■■■■■■ ■■■■", file: "fog4.txt" },
 
     // ===== 居民档案公告 =====
-    { id: "announce1", section: "residents", title: "公告：档案格式与撰写规则", poster: "[管理员]", desc: "■■■■■■■■■■ ■■■■■■■■■■ ■■■■", file: "announce1.txt" }
+    { id: "announce1", section: "residents", title: "公告：档案格式与撰写规则", poster: "[管理员]", desc: "■■■■■■■■■■ ■■■■■■■■■■ ■■■■", file: "announce1.txt" }{ id: "announce1", section: "居民", title: "公告：档案格式与撰写规则", poster: "[管理员]", desc: "■■■■■■■■■■ ■■■■■■■■■■ ■■■■", file: "announce1.txt" }
 ];
 
 // ---- 居民档案OC列表 ----
@@ -45,7 +45,7 @@ const RESIDENTS = [
 ];
 
 // ---- 组织档案列表 ----
-const ORGS = [
+const ORGS = [常量 ORGs = [组织 =[
     { id: "001", name: "暂无", desc: "███" },
     { id: "002", name: "暂无",   desc: "███" }
 ];
